d9909331057f3442ad042b6239c379b9c17415d5
shayla-huff <shxylx221@gmail.com>
2025-10-04 16:59:05 -0400

    hashrouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import Header from './Header';
 import Intro from './Intro';
 import WorkList from './WorkList';
