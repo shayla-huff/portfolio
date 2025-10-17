94892d1c8788c55e9136fe0d67b29d2983ea2454
shayla-huff <shxylx221@gmail.com>
2025-10-17 11:56:37 -0400

    css tweaks

--- src/intro.jsx
+++ src/intro.jsx
@@ -1,6 +1,6 @@
 const Intro = () => {
     return (
-        <section>
+        <section className="intro">
             <h1>ALL THINGS DESIGN</h1>
             <p>I'm a Web Design student at Purdue University, aspiring to become proficient in all things design: web/user interface design, graphic design, branding design, advertising design, </p>
             <button onClick={() => window.location.href = '/contact'}>LET'S TALK</button>
