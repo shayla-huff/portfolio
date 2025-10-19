e84f117a81c6abdfa4237ee91b9ca4cbd7fccb85
shayla-huff <shxylx221@gmail.com>
2025-10-19 12:01:49 -0400

    intro

--- src/intro.jsx
+++ src/intro.jsx
@@ -1,9 +1,13 @@
+import { useNavigate } from "react-router-dom";
+
 const Intro = () => {
+    const navigate = useNavigate();
+
     return (
         <section className="intro">
             <h1>ALL THINGS DESIGN</h1>
             <p>I'm a Web Design student at Purdue University, aspiring to become proficient in all things design: web/user interface design, graphic design, branding design, advertising design, </p>
-            <button onClick={() => window.location.href = '/ContactForm'}>LET'S TALK</button>
+            <button onClick={() => navigate("/contact")}>LET'S TALK</button>
         </section>
     );
 };
