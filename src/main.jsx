74425db0249ffc7b99b59f9e611d3e92d2b8193d
shayla-huff <shxylx221@gmail.com>
2025-10-20 19:57:16 -0400

    +

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { HashRouter as Router } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import App from './App.jsx'
 import './App.css'
 
