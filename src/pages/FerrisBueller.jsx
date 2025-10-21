c38999e43d5c2a88cd801752bf4744e8367626ed
shayla-huff <shxylx221@gmail.com>
2025-10-20 20:27:32 -0400

    css

--- src/pages/FerrisBueller.jsx
+++ src/pages/FerrisBueller.jsx
@@ -4,10 +4,10 @@ const FerrisBueller = () => {
     return (
         <section className="page ferris-bueller">
             <h2>Ferris Bueller Poster</h2>
-            <img src={ferrisImage} alt="Ferris Bueller Poster" className="project-image"/>
             <h3>A mock movie poster made for the well known 1986 classic, <em>Ferris Bueller's Day Off</em> (one of my favorites!)</h3>
             <p>My goal was to advertise the adventurous, youthful vibe of the film through imagery of Chicago, singular shots of the main three cast members, and a cutout from the famous art museum scene.</p>
             <p><em>created for an assignment for CGT 118 at Purdue University</em></p>
+            <img src={ferrisImage} alt="Ferris Bueller Poster" className="project-image"/>
         </section>
     );
 };
