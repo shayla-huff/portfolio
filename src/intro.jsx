0b3183d9358c5e40f3cb01202b745d794062467b
shayla-huff <shxylx221@gmail.com>
2025-10-20 22:58:11 -0400

    ..

--- src/intro.jsx
+++ src/intro.jsx
@@ -6,7 +6,7 @@ const Intro = () => {
     return (
         <section className="intro">
             <h1>ALL THINGS DESIGN</h1>
-            <p>I'm a Web Design student at Purdue University, aspiring to become proficient in all things design: web/user interface design, graphic design, branding design, advertising design, </p>
+            <p>I'm a Web Design student at Purdue University, aspiring to become proficient in all things design: web/user interface design, graphic design, branding design, advertising design, etc. </p>
             <button onClick={() => navigate("/contact")}>LET'S TALK</button>
         </section>
     );
